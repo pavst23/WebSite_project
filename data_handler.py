17de5c4548b7dc0f55301d95f8f1cbd484fb4ad7
Roman Smirnov <smirnov101.23@gmail.com>
2023-04-15 18:48:17 +0300

    Data handler release

--- data_handler.py
+++ data_handler.py
@@ -4,13 +4,36 @@ import json
 
 
 class Handler():
-    def __int__(self):
+    def __init__(self):
         self.connect()
+        self.data_write()
+        self.data_append()
+        self.data_update()
 
     def connect(self):
         gc = gspread.service_account(filename='service_account.json')
-        read_cart_orders = gc.open('Регистрация').get_worksheet(0)
-        cart_orders = read_cart_orders.get_all_values()
-        self.cart_orders_data = pd.DataFrame(cart_orders)
-        reg_info = json.load(open('res.json'))
+        self.read_all_reg = gc.open('Регистрация').get_worksheet(0)
+        self.all_reg = self.read_all_reg.get_all_records()
+        self.all_reg = pd.DataFrame(self.all_reg)
+        self.reg_info = dict(json.load(open('res.json')))
 
+    def data_write(self):
+        data_dict = {}
+        for key, value in self.reg_info.items():
+            data_dict['Name'] = [key]
+            data_dict['Email'] = [value[0]]
+            data_dict['Age'] = [value[1]]
+            data_dict['Sex'] = [value[2]]
+            data_dict['Time'] = [value[3]]
+            data_dict['About'] = [value[4]]
+            data_dict['About2'] = [value[5]]
+            data_dict['File'] = [value[5]]
+        self.new_data = pd.DataFrame(data_dict)
+
+    def data_append(self):
+        self.new_data['Age'] = self.new_data['Age'].astype(object)
+        self.all_reg['Age'] = self.all_reg['Age'].astype(object)
+        self.result_data = self.all_reg._append(self.new_data, ignore_index=True)
+
+    def data_update(self):
+        self.read_all_reg.update([self.result_data.columns.values.tolist()] + self.result_data.values.tolist())
